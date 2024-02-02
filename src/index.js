e4665b3003bafc1511521cd2436f01bde850fb70
Oshea <osheaobajoseph@gmail.com>
2024-02-02 15:59:03 +0100

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <DndProvider backend={HTML5Backend}>
     <App />
-  </React.StrictMode>
+  </DndProvider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
